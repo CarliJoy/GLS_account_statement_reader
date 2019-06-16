7a07a5363d553222a82af1cc7bc85c53398d5dbd
Norman Carl Freudenberg <kound@posteo.de>
2019-06-16 14:35:44 +0200

    refractor booking to own file

--- statement_reader/statement_reader.py
+++ statement_reader/statement_reader.py
@@ -2,46 +2,8 @@ import csv
 import re
 import subprocess
 import tempfile
-from datetime import datetime
+from .bookings import Bookings
 from .booking import Booking
-
-
-class Bookings(list):
-    def __init__(self):
-        super().__init__()
-        self.daterelation = dict()
-
-    def html_filter_entry_without_category(self, filter: bool = True):
-        result = "<table class='table_basic'><tr><th>Date</th><th>Category</th><th>Type</th><th>Amount</th><th>Payee</th><th>Comment</th></tr>"
-        for i in self:
-            if not i.category or not filter:
-                result = f"{result}\n<tr>{i._tr_}</tr>"
-        return f"{result}</table>"
-
-    def _repr_html_(self):
-        return self.html_filter_entry_without_category(False)
-
-    def __add__(self, other: 'Bookings'):
-        result = Bookings()
-        # for key in sorted(self.daterelation.keys() + other.daterelation.keys()):
-        for itm in self:
-            result.append(itm)
-        for itm in other:
-            result.append(itm)
-        return result
-
-    def save(self, filename):
-        with open(filename, "w", newline="\n", encoding="utf-8") as fp:
-            fp.write("Date;Category;Type;Amount;Payee;Comment\n")
-            for i in self:
-                fp.write(f"{i}\n")
-
-    def append(self, booking: Booking):
-        booking_date = datetime.strptime(booking.date, "%d.%m.%Y").date()
-        if not booking_date in self.daterelation:
-            self.daterelation[booking_date] = list()
-        self.daterelation[booking_date].append(booking)
-        super().append(booking)
 
 
 def csv2bookings(filename):
