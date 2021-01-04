d4b7a213191bb38fe205d853b6ffe6b0668d54e1
Carli* Freudenberg <kound@posteo.de>
2021-01-04 18:08:49 +0100

    allow new format

--- statement_reader/statement_reader.py
+++ statement_reader/statement_reader.py
@@ -6,8 +6,31 @@ from logging import getLogger
 
 from .bookings import Bookings
 from .booking import Booking
+from .exceptions import ParsingError
 
 logger = getLogger("statement_reader.reader")
+
+
+def parse_amount_string(amount: str) -> float:
+    """
+    Parse strings like
+     - '1.000,00-'
+     - '1.000,00   S'
+     - '23,23+'
+     - '23,23     H'
+    to the correct amount as float
+    :param amount: The string of an amount
+    :return:
+    """
+    # Replace H and S
+    amount = amount.replace('H', '+').replace('S',  '-')
+    # Remove german thousand separator
+    amount = amount.replace('.', '')
+    # Replace german decimal separator with english
+    amount = amount.replace(',', '.')
+    # Put the sign at the correct place and trim
+    amount = amount[-1] + amount[:-1].strip()
+    return float(amount)
 
 
 def csv2bookings(filename):
@@ -74,11 +97,38 @@ def data2booking(data, year):
                 booking.payee = payee
                 bookings.append(booking, ignore_duplicates=False)
             booking = Booking()
+            """
+            Example for new file
+            '02.01.    01.01. Überweisungsgutschr.                                                                                          1.000,00 H'
+            Example for old file
+            '01.04.   Dauer-Euro-Überweisung                                                    1,00-'
+            
+            """
+            #re.sub("(,[0-9][0-9][ ]+)([H])",)
+
             vals = re.split("[ ]{4,100}", line)
-            # print(vals)
-            booking.date = f"{vals[0]}{year}"
-            booking.type = vals[1]
-            booking.amount = float(vals[2][-1:] + vals[2][:-1].replace(".", "").replace(",", "."))
+            matches = re.fullmatch(
+                '(?P<date1>[0-9]{2}[.][0-9]{2}[.])'
+                '([ ]+(?P<date2>[0-9]{2}[.][0-9]{2}[.]))?'
+                '[ ]*(?P<type>[^0-9]+)'
+                '(?P<amount>[0-9,.]+[ ]*[HS+-])',
+                line
+            )
+            if matches is None:
+                raise ParsingError(
+                    f"Could not parse the line: \n"
+                    f"  '{line}'\n"
+                    f"It seem not to follow the format of a typical bank report"
+                )
+            matches = matches.groupdict()
+            if matches['date2'] is not None:
+                # Always use the
+                date = matches['date2']
+            else:
+                date = matches['date1']
+            booking.date = f"{date}{year}"
+            booking.type = matches['type'].strip()
+            booking.amount = parse_amount_string(matches['amount'])
             booking.comment = ""
             next_is_payee = True
         elif next_is_payee:
@@ -92,23 +142,30 @@ def data2booking(data, year):
 
 def pdf2bookings(filepath):
     with tempfile.NamedTemporaryFile() as tmpfile:
-        # First convert the pdf to text keeping the layout - all output is handeled in a temporary file that is deleted afters
+        # First convert the pdf to text keeping the layout -
+        # all output is handeled in a temporary file that is deleted afters
         subprocess.run(['pdftotext', '-layout', filepath, tmpfile.name])
         # extract the year
         result = subprocess.run(
             f"cat {tmpfile.name} "
             "| sed -n -e '/erstellt am [23][0-9][.][01][0-9].20[0-9][0-9]/ {{p;q}}'"
-            " | sed 's/^.*am [23][0-9][.][01][0-9].\\(20[0-9][0-9]\\)/\\1/'",
+            "| sed 's/^.*am [23][0-9][.][01][0-9].\\(20[0-9][0-9]\\).*/\\1/'",
             shell=True, stdout=subprocess.PIPE, encoding="UTF-8"
         )
         year = result.stdout.replace("\n", "").strip()
+        if len(year) != 4:
+            raise ParsingError(
+                f"Could not extract year from {filepath}. "
+                f"Extracted {year} which seems not to be a correct year."
+            )
         # Now just extract the actual lines containing a transfer
         # by looking at every block that starts with the date
         # the first sed is used to remove all white lines before a date for easier
         # processing
         result = subprocess.run(
-            f"cat {tmpfile.name} | sed 's/^[ ]*\\([0-3][0-9][.][0-1][0-9].[ ]*\\) /\\1    /'"
-            "| sed -ne '/[0-3][0-9][.][0-1][0-9].[ ]\\{{1,4\\}}/,/^[_ \\t-]*\(SALDO NEU.*\)\{{0,1\}}$/ p'"
+            f"cat {tmpfile.name} "
+            "| sed 's/^[ ]*\\([0-3][0-9][.][0-1][0-9].[ ]*\\) /\\1    /'"
+            "| sed -ne '/[0-3][0-9][.][0-1][0-9].[ ]\\{{1,4\\}}/,/^[_ \\t-]*\\(SALDO NEU.*\\)\{{0,1\}}$/ p'"
             "| sed '/^[_ \\t-]*$/ d'",
             shell=True, stdout=subprocess.PIPE, encoding="UTF-8"
         )
